bcdd51e7d5ebbbed8977beb79a6e3e1bf28bf979
Jahnavichava30 <142965078+Jahnavichava30@users.noreply.github.com>
2023-11-28 20:34:07 -0600

    added login and register pugs

--- app.js
+++ app.js
@@ -68,6 +68,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session())
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -77,6 +87,13 @@ app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/costume', costumeRouter);
 app.use('/resource', resourceRouter);
+
+// passport config
+// Use the existing connection
+// The Account model
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser())
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -93,19 +110,6 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.use(require('express-session')({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session())
 
-// passport config
-// Use the existing connection
-// The Account model
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser())
 
 module.exports = app;
