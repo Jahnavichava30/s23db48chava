ddadecb94eefc20a4cb1f516c5e6e2e361fcdfa3
Jahnavichava30 <142965078+Jahnavichava30@users.noreply.github.com>
2023-11-29 19:11:10 -0600

    done with validation

--- models/hats.js
+++ models/hats.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose")
 const hatSchema = mongoose.Schema({
 costume_type: String,
 size: String,
-cost: Number
+cost: {
+    type: Number,
+    required: true,
+    min: 10, 
+    max: 700,
+  }
 })
 module.exports = mongoose.model("Hats",
 hatSchema)
